6f269294bb81d8459f572b7d29fdd485541448d3
Andrew Wood <andygwood@gmail.com>
2014-10-17 16:26:00 -0400

    initial commit

---
+++ src/Agwood/SocketrClient/SocketrClientServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Agwood\SocketrClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class SocketrClientServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
